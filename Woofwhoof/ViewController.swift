6c85847666ce80b731529287482cb939aab01029
Kirill Ki <kirillkalapov@gmail.com>
2021-06-01 16:28:48 +0200

    First

--- Woofwhoof/ViewController.swift
+++ Woofwhoof/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var animalSoundLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func dogButtonDidPressed(_ sender: Any) {
+        animalSoundLabel.text = "Woof!"
+    }
+    
+    @IBAction func catBottonDidPressed(_ sender: Any) {
+        animalSoundLabel.text = "Meow!"
+    }
+    
+    @IBAction func cowButtonDidPressed(_ sender: Any) {
+        animalSoundLabel.text = "Moo!"
+    }
+    
 }
 
